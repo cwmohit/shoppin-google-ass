a65ebb2e170d70d8c08b4cc1b538aaecd7f56b8c
Mohit kandhari <mohit.k@jiraaf.com>
2024-12-19 00:57:16 +0530

    mohit | init

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import SearchInput from "@/components/SeachInput";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-[rgb(32,33,36,68%)]">
-      <section className="flex-grow relative flex flex-col items-center mt-[280px] w-full max-w-screen-sm mx-auto">
+    <div className="flex flex-col min-h-screen bg-[rgb(32,33,36,68%)] px-4">
+      <section className="flex-grow relative flex flex-col items-center mt-40 md:mt-[280px] w-full max-w-screen-sm mx-auto">
         <div className="mb-8">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png"
